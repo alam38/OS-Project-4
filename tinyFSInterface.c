f28ed1644f44136b69e3d1fbfa6c35a1c5b3306c
Daniel <dpaje@calpoly.edu>
2017-03-19 19:57:55 -0700

    Edited two of the functions in the interface

--- tinyFSInterface.c
+++ tinyFSInterface.c
@@ -24,20 +24,22 @@ int tfs_mount(char *diskname){
    tfs = openDisk(diskname, 
    
 }
-int tfs_umount(void)
-fileDescriptor tfs_openFile(char *name)
+int tfs_umount(void);
+fileDescriptor tfs_openFile(char *name);
    
-int tfs_closeFile(fileDescriptor FD)
+int tfs_closeFile(fileDescriptor FD);
 
-int tfs_writeFile(fileDescriptor FD, char *buffer, int size){
+int tfs_writeFile(fileDescriptor FD, char *buffer, int size){ // cannot figure out how to copy over the buffer one block size at a time
    int i = 0, loops = size / BLOCKSIZE;
+   char *copyBuffer = malloc(4 * BLOCKSIZE);
+
    while(i < loops){
-      if(writeBlock(FD, i, buffer) == -1){
+      memcpy(copyBuffer, buffer, BLOCKSIZE * 4); 
+      if(writeBlock(FD, i, copyBuffer) == -1){
          return -2;
       }
-      buffer = buffer >> BLOCKSIZE;
    }
-   
+   free(copyBuffer);
    return 0;
 }
 
@@ -49,6 +51,7 @@ int tfs_readByte(fileDescriptor FD, char *buffer){
    if(read(FD, buffer, 1) == 0){
       return -1;
    }
+   // file cursor is automatically adjusted when read does work
    return 0;  
 }
 
