2a12145428f951717a35e453497aae9c4ab6ef1d
Daniel <dpaje@calpoly.edu>
2017-03-19 21:56:45 -0700

    Skeleton done for writeFile?

--- tinyFSInterface.c
+++ tinyFSInterface.c
@@ -1,4 +1,5 @@
 #define SUPERBLOCK_MNUMBER 0x44
+#define SUPERBLOCK_MNUMBER 0x44
 #define SUPERBLOCK 1
 #define INODE 2
 #define FILE_EXTENT 3
@@ -17,29 +18,34 @@ int tfs_mkfs(char *filename, int nBytes){
    
    
 }
-int tfs_mount(char *diskname){
+/*int tfs_mount(char *diskname){
    int status, mNum, tfs;
    char* buffer;
    
    tfs = openDisk(diskname, 
    
-}
-int tfs_umount(void);
+}*/
+
+//int tfs_umount(void);
 fileDescriptor tfs_openFile(char *name);
    
 int tfs_closeFile(fileDescriptor FD);
 
-int tfs_writeFile(fileDescriptor FD, char *buffer, int size){ // cannot figure out how to copy over the buffer one block size at a time
+//block writing happens in full bytes
+//buffer contains an entire file's contents
+int tfs_writeFile(fileDescriptor FD, char *buffer, int size){ 
    int i = 0, loops = size / BLOCKSIZE;
-   char *copyBuffer = malloc(4 * BLOCKSIZE);
+   char *copyBuffer = malloc(BLOCKSIZE);
 
    while(i < loops){
-      memcpy(copyBuffer, buffer, BLOCKSIZE * 4); 
+      memcpy(copyBuffer, buffer, BLOCKSIZE); 
       if(writeBlock(FD, i, copyBuffer) == -1){
          return -2;
       }
+      buffer = buffer >> BLOCKSIZE*4
    }
    free(copyBuffer);
+   lseek(FD, 0, SEEK_SET);
    return 0;
 }
 
